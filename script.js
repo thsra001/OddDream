ff5d371a107b06ebfcaebf47b8bcd74b0c5079fc
Thsra001 <44636308+thsra001@users.noreply.github.com>
2021-10-20 12:06:57 +0000

    cleaned this

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // The three.js scene: the 3D world where you put objects
 const scene = new THREE.Scene();
-
 // The camera
 const camera = new THREE.PerspectiveCamera(
   70,
@@ -8,23 +7,28 @@ const camera = new THREE.PerspectiveCamera(
   1,
   10000
 );
-
+//skybox
+const loader = new THREE.CubeTextureLoader();
+const skyboss = loader.load( [
+	'tex/box/px.png', 'tex/box/nx.png',
+	'tex/box/py.png', 'tex/box/ny.png',
+	'tex/box/pz.png', 'tex/box/nz.png'
+] );
 // The renderer: something that draws 3D objects onto the canvas
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xaaaaaa, 1);
 // Append the renderer canvas into <body>
 document.body.appendChild(renderer.domElement);
-
-
-// A cube we are going to animate
-const texture = new THREE.TextureLoader().load( 'carpet.jpg' );
+//loading textures
+const texture = new THREE.TextureLoader().load( 'tex/carpet.jpg' );
 texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(2,2)
-const bg = new THREE.TextureLoader().load( 'sky.jpg' );
+//lighting
 const ambient = new THREE.AmbientLight(0x898989)
 const light = new THREE.HemisphereLight( 0x93d1f5, 0xa16508, 1 );
 light.position.set(0,2,0)
+// shapes to put on screen
 const cube2 = {
   // The geometry: the shape & size of the object
   geometry: new THREE.BoxGeometry(0.2, 6, 18),
@@ -38,7 +42,7 @@ const floor = {
   // The material: the appearance (color, texture) of the object
   material: new THREE.MeshBasicMaterial ({ map: texture})
 };
-// The mesh: the geometry and material combined, and something we can directly add into the scene (I had to put this line outside of the object literal, so that I could use the geometry and material properties)
+// The mesh: the geometry and material combined, to add to scene 
 cube.mesh = new THREE.Mesh(cube2.geometry, cube2.material);
 cube2.mesh = new THREE.Mesh(cube2.geometry, cube2.material);
 floor.mesh = new THREE.Mesh(floor.geometry, floor.material);
@@ -47,20 +51,15 @@ scene.add(cube.mesh);
 scene.add(floor.mesh)
 scene.add(cube2.mesh);
 scene.add(light);
-scene.background = bg;
+scene.background = skyboss
+// positioning
 cube.mesh.position.x=-3
 cube2.mesh.position.x=3
 floor.mesh.position.y=-3
-// Make the camera further from the cube so we can see it better
-camera.position.z = 5;
-// skybox
-const loader = new THREE.CubeTextureLoader();
-const skyboss = loader.load( [
-	'tex/box/px.png', 'tex/box/nx.png',
-	'tex/box/py.png', 'tex/box/ny.png',
-	'tex/box/pz.png', 'tex/box/nz.png'
-] );
-  scene.background = skyboss
+
+
+
+//render loop for rendering scene and logic loop
 function render() {
   // Render the scene and the camera
   renderer.render(scene, camera);
